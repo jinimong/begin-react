7881eee4016a4e8e55e062d67a6c70e0fa0aeeab
jwkim <phoongdeng@gmail.com>
2020-06-05 16:04:32 +0900

    useState 를 활용하여 input 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
-  return <Counter />;
+  return <InputSample />;
 }
 
 export default App;

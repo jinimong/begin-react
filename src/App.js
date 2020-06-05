b415b6e7e390fc8ed6cb5c457a0b65233e2d13f3
jwkim <phoongdeng@gmail.com>
2020-06-05 15:17:18 +0900

    props.children 을 통한 Wrapper 컴포넌트 예제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem',
-  };
-
   return (
-    <>
-      {/* 주석은 화면에 보이지 않습니다 */}
+    <Wrapper>
       <Hello name="react" color="red" />
       <Hello color="pink" />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    </Wrapper>
   );
 }
 

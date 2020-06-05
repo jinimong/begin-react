3ba5b2e39d9006240c01af2cb2df1f32f58d2dce
jwkim <phoongdeng@gmail.com>
2020-06-05 15:57:23 +0900

    useState 의 setter 함수에 콜백함수 넘기기

--- src/Counter.js
+++ src/Counter.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 function Counter() {
   const [number, setNumber] = useState(0);
   const onIncrease = () => {
-    setNumber(number + 1);
+    setNumber((prevNumber) => prevNumber + 1);
   };
   const onDecrease = () => {
-    setNumber(number - 1);
+    setNumber((prevNumber) => prevNumber - 1);
   };
   return (
     <div>

9b3e272574a2ae761f53d2653192848da14f92e0
jwkim <phoongdeng@gmail.com>
2020-11-25 03:24:05 +0900

    Add options (outline, fullWidth)

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,6 +13,17 @@ const colorStyles = css`
       &:active {
         background: ${darken(0.1, selected)};
       }
+      ${(props) =>
+        props.outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
     `;
   }}
 `;
@@ -39,6 +50,19 @@ const sizeStyles = css`
   `}
 `;
 
+const fullWidthStyle = css`
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+      justify-content: center;
+      &:not(:first-child) {
+        margin-left: 0;
+        margin-top: 1rem;
+      }
+    `}
+`;
+
 const StyledButton = styled.button`
   /* 공통 스타일 */
   display: inline-flex;
@@ -58,14 +82,21 @@ const StyledButton = styled.button`
   ${colorStyles}
 
   /* 기타 */
-  & + & {
+  &:not(:first-child) {
     margin-left: 1rem;
   }
+  ${fullWidthStyle}
 `;
 
-function Button({ children, color, size, ...rest }) {
+function Button({ children, color, size, outline, fullWidth, ...rest }) {
   return (
-    <StyledButton color={color} size={size} {...rest}>
+    <StyledButton
+      color={color}
+      size={size}
+      outline={outline}
+      fullWidth={fullWidth}
+      {...rest}
+    >
       {children}
     </StyledButton>
   );
